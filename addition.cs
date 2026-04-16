55ea5a85d06cd2d9e38cecc6a011ab504295d6d1
banavathuganesh39-cmd <banavathuganesh39@gmail.com>
2026-04-16 08:54:26 +0000

    created

---
+++ addition.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Add the numbers
+        int sum = num1 + num2;
+        
+        // Display the result
+        Console.WriteLine("Number 1: " + num1);
+        Console.WriteLine("Number 2: " + num2);
+        Console.WriteLine("Sum: " + sum);
+    }
+}
